f12d3f8965387dc6fb6c1200a7e2a12f0b27a939
yangzhiyuan0127 <2675882144@qq.com>
2020-11-13 14:01:58 +0800

    header

---
+++ src/assets/js/rem.js
@@ -0,0 +1,30 @@
+;
+(function(win) {
+    var doc = win.document;
+    var docEl = doc.documentElement; 
+    var tid;
+
+    function refreshRem() {
+        // 获取屏幕宽度
+        var width = docEl.getBoundingClientRect().width;
+        var rem = width / 7.5; // 将屏幕宽度分成6.4份， 1份为1rem   320/6.4 = 50 
+        // 让html的fontSize = 50px   1rem = 50px
+        docEl.style.fontSize = rem + 'px';
+        // 320  /6.4 = 50   html->50      1rem   50 
+        // 640  /6.4 = 100  html-> 100           100
+    }
+
+    win.addEventListener('resize', function() {
+        clearTimeout(tid);
+        tid = setTimeout(refreshRem, 10);
+    }, false);
+    win.addEventListener('pageshow', function(e) {
+        if (e.persisted) {
+            clearTimeout(tid);
+            tid = setTimeout(refreshRem, 10);
+        }
+    }, false);
+
+    refreshRem();
+
+})(window);
--- src/views/home/Head.vue
+++ src/views/home/Head.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-<input type='text'>
+<input type='text' placeholder="请输入商品名称">
 
     </div>
 </template>
@@ -13,8 +13,17 @@
 
 <style lang="scss" scoped>
 .container{
-    height: 50;
     background: #101b20;
+    text-align: center;
+    padding:0.2rem 0;
+    input{
+       width: 90%; 
+       height:0.6rem;
+       border: none;
+       border-radius:0.3rem;
+       outline:none;
+       text-indent: 20px;
+    }
 }
 
 </style>
