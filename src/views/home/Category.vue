387778256c7679333183dd293748e87060944bae
yangzhiyuan0127 <2675882144@qq.com>
2020-11-13 16:41:32 +0800

    zhongjian

--- src/views/home/Category.vue
+++ src/views/home/Category.vue
@@ -1,15 +1,51 @@
 <template>
     <div>
-啦啦啦
+        <ul class="list-box">
+            <li class="list" v-for="obj in cateList" :key="obj.id">
+                <img :src="obj.img" alt="">
+            <p>{{obj.name}}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
-        
+        data(){
+            return{
+                cateList:[]
+            }
+        },
+        created(){
+            axios.get('http://admin.gxxmglzx.com/tender/test/get_type')
+        .then((res)=>{
+          //返回数据在res.data中
+          console.log(res.data)
+          let result=res.data;
+          if(result.errcode==200){
+               this.cateList=result.data;
+          }
+        }).catch((error)=>{
+            console.log(error)
+           })
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+    
+    .list-box{
+       display:flex;
+       flex-wrap: wrap;
 
+    }
+    .list{
+        width: 20%;
+        text-align: center;
+        margin-top: 0.3rem;
+    }
+    .list img{
+        width: 60%;
+    }
 </style>
