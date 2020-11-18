76bcff4a7e868cb9ddef2ebe58d25bfcb9d07334
yangzhiyuan0127 <2675882144@qq.com>
2020-11-18 14:12:31 +0800

    异步加载

---
+++ src/components/Star.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <ul>
+           <li v-for="i in 5" :key="i" :class="{
+               'active':i<num,
+           
+               'unactive':i>num,
+           }" ></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+          num:{
+            type:Number,
+            default:5
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+ul{
+    display:flex;
+    li{
+         width: 12px;
+         height: 12px;
+         background-size:12px 12px;
+    }
+    .active{
+         background-image: url(./images/star-active.png);
+        
+    }
+    .half-active{
+         background-image:url(./images/star-half.png);
+    }
+    .unactive{
+        background-image:url(./images/star.png) ;
+    }
+}
+</style>
--- src/views/home/List.vue
+++ src/views/home/List.vue
@@ -1,26 +1,74 @@
 <template>
     <div>
 <ul class="store-list-box">
-    <li class="store-list">
-        <img src="" alt="">
+    <li class="store-list" v-for="obj in list" :key="obj.id">
+        <img class="store-img" :src="obj.img" alt="">
          <div class="store-info">
-             <h2>奈雪的茶</h2>
-             <div>月销1000+</div>
-             <div>配送</div>
+             <h2>{{obj.name}}</h2>
+             <div>
+                 <star num="3"></star>
+                 {{obj.num}}</div>
+             <div>配送时间：{{obj.minute}}</div>
          </div>
     </li>
 </ul>
     </div>
 </template>
-
 <script>
+import axios from 'axios'
+import Star from '@/components/Star'
     export default {
-        
-    }
+        data(){
+            return{
+                list:[],
+                Num:0
+            }
+        },
+        components:{
+          Star
+        },
+        methods:{
+     getData(){
+      axios.get("http://admin.gxxmglzx.com/tender/test/get_store?current="+this.Num+"&size=10")
+     .then((res)=>{
+       // console.log(res.data.data.list);
+        this.list= [...this.list,...res.data.data.list]
+        this.Num++;
+    }).catch((err)=>{
+        console.log(err); 
+            })  
+        }
+    },
+        //获取列表的数据
+    created(){
+
+        this.getData()
+
+        window.onscroll=() =>{
+            let scrollTtop=document.documentElement.scrollTop;
+            let clientHeight=document.documentElement.clientHeight;
+            let scrollHeight=document.documentElement.scrollHeight;
+        if(scrollTtop+clientHeight==scrollHeight){
+        this.getData();
+                }
+            }
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
+.store-list-box{
+    padding: 0.2rem;
    .store-list{
+       //父元素flex子元素横置
        display: flex;
+       .store-img{
+           width:1.2rem;
+       }
+    .store-info{
+        flex:1;
+    }
    }
+}
+
 </style>
